64df99cf3aed0aab1b4a15c9e74d8b4eecfaa8e2
Axel <clever.axel95@gmail.com>
2024-01-28 17:52:25 +0100

    power shell script garbage collector

---
+++ GarbageCollector.ps1
@@ -0,0 +1,47 @@
+ param (
+    [switch]$force = $false
+ )
+
+
+$pathToTemp = "C:\PHPProjects\PHPChunkUpload\upload\temp"
+$formatDate = "+%Y-%m-%d %H:%M:%S"
+$maxTimeLifeSpan = 60 * 60 * 8
+
+if (-NOT (Test-Path -Path $pathToTemp)) {
+    Write-Error "Temporary folder doesn't exist. Exiting garbage collector"
+    exit
+}
+
+Set-Location -Path $pathToTemp
+# Write-Host (Get-Location)
+
+$startUnixTimeStamp = (Get-Date -Date "1970-01-01" -UFormat $formatDate);
+$currentTimeStamp = (Get-Date -Date (Get-Date).ToUniversalTime() -UFormat $formatDate);
+
+$currentUnixTimeStamp = ([int64](New-TimeSpan -Start $startUnixTimeStamp -End $currentTimeStamp).TotalSeconds)
+
+$directories = Get-ChildItem -Directory -Name
+
+foreach ($directory in $directories){
+    $relaTivedirectory = ".\" + $directory
+    $files = Get-ChildItem -Path $relaTivedirectory -File -Name -Include *.json
+
+    foreach ($file in $files){
+        $lastWriteTime = (Get-item ($relaTivedirectory + "\" + $file)).LastWriteTime
+        $lastWriteTime = (Get-Date ($lastWriteTime).ToUniversalTime() -UFormat $formatDate)
+        $lastWriteTime = ([int64](New-TimeSpan -Start $startUnixTimeStamp -End $lastWriteTime).TotalSeconds)
+
+        if((($currentUnixTimeStamp - $lastWriteTime) -gt $maxTimeLifeSpan) -or $force){
+            if(Test-Path -Path ($pathToTemp + "\" +$directory)){
+                Remove-Item -Path ($pathToTemp + "\" +$directory) -Force -Recurse
+                # Write-Host "Deleted temporary directory : "($pathToTemp + $directory)
+            } else {
+                Write-Warning "Temporary directory doesn't exist"
+            }
+        } else {
+            # Write-Host "Upload still active for :" ($pathToTemp + $directory)
+        }
+    }
+}
+
+# powershell -executionpolicy bypass -file .\GarbageCollector.ps1
--- test.ps1
+++ test.ps1
@@ -2,9 +2,21 @@ Write-Host "hello";
 # Read-Host -Prompt "Press any key to continue"
 # $files = Get-ChildItem -Directory -Name
 $files = Get-ChildItem -File -Name -Exclude *.json
+Write-Host (Get-Date -Date "1970/01/01" -UFormat '+%Y-%m-%d %H:%M:%S')
+exit;
+
+# $date1 = Get-Date -Date "01/01/1970"
+# $date2 = Get-Date
+# Write-Host ([int64](New-TimeSpan -Start $date1 -End $date2).TotalSeconds)
 
 foreach ($f in $files){
-    Write-host $f
+    # Write-host (Get-item $f).LastWriteTime
+    $stuff = (Get-item $f).LastWriteTime
+    Write-Host (Get-Date ($stuff).ToUniversalTime() -UFormat '+%Y-%m-%dT%H:%M:%S.000Z')
+
+    # $totalSeconds =  (Get-item $f).LastWriteTime.TotalSeconds
+    # Write-Host $totalSeconds
+
     # Get-Content $f.FullName | Where-Object { ($_ -match 'step4' -or $_ -match 'step9') } | Set-Content $outfile
 }
 
